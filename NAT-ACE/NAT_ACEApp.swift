b81c939b65fe9f9f8ac1af09698634ad639f893c
Didier <did35@yahoo.com>
2024-07-02 15:55:24 +0200

    Initial Commit

---
+++ NAT-ACE/NAT_ACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NAT_ACEApp.swift
+//  NAT-ACE
+//
+//  Created by Didier Delhaisse on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NAT_ACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
