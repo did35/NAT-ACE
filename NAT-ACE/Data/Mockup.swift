ddca8ef826aa8319e62c4854b8d9d6c53f82ed4b
Didier <did35@yahoo.com>
2024-07-08 11:39:18 +0200

    [Feature] amo.json updated til 1893.

---
+++ NAT-ACE/Data/Mockup.swift
@@ -0,0 +1,61 @@
+//
+//  Mockup.swift
+//  NAT-ACE
+//
+//  Created by Didier Delhaisse on 08/07/2024.
+//
+
+import Foundation
+
+struct Mockup {
+    /*
+     {
+         "year": "189-01",
+         "value": 0.0
+     },
+     {
+         "year": "189-02",
+         "value": 0.0
+     },
+     {
+         "year": "189-03",
+         "value": 0.0
+     },
+     {
+         "year": "189-04",
+         "value": 0.0
+     },
+     {
+         "year": "189-05",
+         "value": 0.0
+     },
+     {
+         "year": "189-06",
+         "value": 0.0
+     },
+     {
+         "year": "189-07",
+         "value": 0.0
+     },
+     {
+         "year": "189-08",
+         "value": 0.0
+     },
+     {
+         "year": "189-09",
+         "value": 0.0
+     },
+     {
+         "year": "189-10",
+         "value": 0.0
+     },
+     {
+         "year": "189-11",
+         "value": 0.0
+     },
+     {
+         "year": "189-12",
+         "value": 0.0
+     }
+     */
+}
